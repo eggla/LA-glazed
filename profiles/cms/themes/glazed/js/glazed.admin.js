c7b21b1113bcbbdf02b377151cbc8a05cc979121
Brendan Ryan <exitglacierguides@yahoo.com>
2017-01-18 17:02:42 -0900

    Post Theme Update + Trip revisions

--- profiles/cms/themes/glazed/js/glazed.admin.js
+++ profiles/cms/themes/glazed/js/glazed.admin.js
@@ -24,12 +24,13 @@
       }
 
       function glazedButtonAdd($toolbar) {
+        var themeName = Drupal.settings.glazedDefaultTheme || 'glazed';
         var glazedLogoPath = Drupal.settings.basePath + Drupal.settings.glazed.glazedPath + 'glazed-favicon.png';
         var $glazedButton = $('<div id="glazed-button-wrapper">').html($('<a>',{
-          text: Drupal.t('Glazed Settings'),
-          title: 'Glazed Settings, Demo Import, and more',
+          text: Drupal.t('Theme Settings'),
+          title: 'Theme Settings, Demo Import, and more',
           class: 'glazed-button',
-          href: Drupal.settings.basePath + 'admin/appearance/settings/glazed'
+          href: Drupal.settings.basePath + 'admin/appearance/settings/' + themeName
         }).prepend($('<img>',{
           src: glazedLogoPath,
           width: 15
