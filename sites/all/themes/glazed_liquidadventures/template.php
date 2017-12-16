9983888059421519ffbc09152e63dc40dd6b0847
Brendan Ryan <exitglacierguides@yahoo.com>
2017-12-16 12:35:47 -0900

    Map and menu changes

--- sites/all/themes/glazed_liquidadventures/template.php
+++ sites/all/themes/glazed_liquidadventures/template.php
@@ -24,3 +24,8 @@ function glazed_liquidadventures_process_page(&$vars) {
     _color_page_alter($vars);
   }
 }
+
+function glazed_liquidadventures_link($variables) {
+	$variables['options']['html'] = TRUE;
+  return '<a href="' . check_plain(url($variables['path'], $variables['options'])) . '"' . drupal_attributes($variables['options']['attributes']) . '>' . ($variables['options']['html'] ? $variables['text'] : check_plain($variables['text'])) . '</a>';
+}
